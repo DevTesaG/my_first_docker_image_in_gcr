20a2401d353bed2bf3b926262e4b5dd4226772fc
DevTesaG <feynman.stories@gmail.com>
2022-10-25 15:16:13 -0500

    changed console.log app for a simple express app

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
-FROM node:alpine
-COPY . /app
-WORKDIR /app
+FROM node:14-alpine3.12
+
+WORKDIR /usr/src/app
+COPY package*.json ./
+RUN npm install 
+COPY . .
+EXPOSE 8080
 CMD node app.js
--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hello world from github container registry. ALIVE N' RUNNING")
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", async (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.status(200);
+  res.send("<h1>Hello world</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("good");
+  }, 300);
+  reject("bad");
+});
+
+myPromise.then(() => {
+  console.log("this will never run");
+});
